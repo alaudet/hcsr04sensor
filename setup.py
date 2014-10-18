50ea47e86a1d125531ac6c48c0ec9b571942e347
Al Audet <alaudet@linuxnorth.org>
2014-10-18 16:53:46 -0400

    add license, manfigest.in

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 import os
-version = '0.0.1'
+version = '1.0.0'
 
 
 setup(name='hcsr04sensor',
@@ -8,7 +8,7 @@ setup(name='hcsr04sensor',
       description='Simple module to access HCSR04 sensor on RaspiPi',
       long_description=open("./README.md", "r").read(),
       classifiers=[
-          "Development Status :: 4 - Beta",
+          "Development Status :: 5 - Production/Stable",
           "Environment :: Console",
           "Intended Audience :: End Users/Desktop",
           "Natural Language :: English",
@@ -19,8 +19,8 @@ setup(name='hcsr04sensor',
           ],
       author='Al Audet',
       author_email='alaudet@linuxnorth.org',
-      url='http://www.linuxnorth.org/',
-      download_url='https://github.com/alaudet/',
+      url='https://github.com/alaudet/hcsr04sensor',
+      download_url='https://github.com/alaudet/hcsr04sensor',
       license='MIT License',
       packages=['hcsr04sensor'],
       install_requires=['RPi.GPIO']
